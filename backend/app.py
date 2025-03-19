f139afd7f0b03e64e77487b6672b60d54c43ae87
Skyler Shapiro <ss2879@cornell.edu>
2025-03-18 23:12:40 -0400

    created file retrieval.py to handle information retrieval. Haven't actually implemented this yet.

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+import retrieval
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
---
+++ backend/retrieval.py
@@ -0,0 +1,8 @@
+#This script handle's the information retrieval component of this project. 
+#User queries will be sent from the frontend ---> the backend.
+#This script will find a set of relevant documents in our dataset using this query
+    #As we add complexity to the kinds of queries our system tolerates, we should build 
+    #a "parse_query.py" that smoothly passes Frontend output into this retrieval system in the backend
+
+#SIMPLIFIED FOR TA MEETING 3/19
+query = 'oily skin, cerave, cetaphil'
