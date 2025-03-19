ee60674b9665b1822fc3c035a9c66662c0646345
Skyler Shapiro <ss2879@cornell.edu>
2025-03-18 23:38:05 -0400

    added streamlit to venv, adding skeleton code and documentation for info retrieval

--- backend/retrieval.py
+++ backend/retrieval.py
@@ -4,5 +4,30 @@
     #As we add complexity to the kinds of queries our system tolerates, we should build 
     #a "parse_query.py" that smoothly passes Frontend output into this retrieval system in the backend
 
-#SIMPLIFIED FOR TA MEETING 3/19
-query = 'oily skin, cerave, cetaphil'
+#SIMPLIFIED IMPLEMENTATION for TA meeting on 3/19
+
+def process_json():
+    '''This function recieves JSON output from frontend and parses into suitable format to conduct information retrieval'''
+    pass
+
+
+tags = ["moisturizer"] 
+'''
+Type: List of strings
+Currently written as 'Skin Concerns' in frontend. '''
+
+price_range = (0,200) 
+'''
+Type: Tuple(int1, int2) with int1 <= int2
+WHEN NOT SET BY USER, default will be 0-200, 
+If this is the case, maybe we can just ignore price-range during retrieval? '''
+
+exact_product_search = 'CeraVe Daily Moisturizing Lotion for Dry Skin, Body Lotion & Face Moisturizer with Hyaluronic Acid and Ceramides'
+'''
+Type: String
+User looking for specific product, free-text search term
+Should be able to tolerate miss-spellings and near-perfect hits (fuzzy match). '''
+
+# logic to determine how to conduct retrieval
+
+
