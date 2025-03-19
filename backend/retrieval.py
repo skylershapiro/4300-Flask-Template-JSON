229f121b71ef3a6db14e96112b8ea212482acfc8
Skyler Shapiro <ss2879@cornell.edu>
2025-03-19 00:38:01 -0400

    added very basic recommender that uses edit distance between user query and product name to find relevant documents.

--- backend/retrieval.py
+++ backend/retrieval.py
@@ -6,7 +6,7 @@
 
 #SIMPLIFIED IMPLEMENTATION for TA meeting on 3/19
 
-def process_json():
+def parse_json():
     '''This function recieves JSON output from frontend and parses into suitable format to conduct information retrieval'''
     pass
 
@@ -30,4 +30,35 @@ Should be able to tolerate miss-spellings and near-perfect hits (fuzzy match). '
 
 # logic to determine how to conduct retrieval
 
+
+#Determine how to build search criteria
+import nltk
+import pandas as pd
+
+use_tags = False
+use_price_range = False
+use_exact_product_search = False
+
+if tags != "": use_tags = True
+if price_range != (1,200): use_price_range = True
+if exact_product_search != "": use_exact_product_search = True
+
+# find 20 most relevant documents using levenshtein edit distance between user free-text queries and product names
+
+# read in data
+df = pd.read_csv("/Users/skylershapiro/cs4300/skincare_products_clean.csv")
+df = df[["product_name", "product_type", "price"]]
+
+relevant_doc_inds = []
+i = 0
+for d in df['product_name']:
+   i += 1 
+   sim = nltk.edit_distance(exact_product_search, d.split()) # calculate similiarty between query and product name
+   if sim > 0.6: # throw out obvious poor matches
+       relevant_doc_inds.append((d, sim)) # store document and similarity score
+
+# return top 20 matches
+top_20_relevant_docs = sorted(relevant_doc_inds,  key=lambda x: x[1])[-3:]
+print(top_20_relevant_docs)
+       
 
