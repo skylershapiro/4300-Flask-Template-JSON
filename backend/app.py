56d35ceab0b3f4d0ebcf36529528a6e19d411108
Jordan <99847889+jordannnhan@users.noreply.github.com>
2025-03-21 17:36:25 -0400

    changed the port to 5225

--- backend/app.py
+++ backend/app.py
@@ -98,5 +98,5 @@ def search():
 
 if __name__ == '__main__':
     # app.run(debug=True, host="0.0.0.0", port=5013)
-    port = int(os.environ.get("PORT", 5013))  # Use PORT if set, otherwise default to 5000
+    port = int(os.environ.get("PORT", 5225))  # Use PORT if set, otherwise default to 5000
     app.run(host="0.0.0.0", port=port)
