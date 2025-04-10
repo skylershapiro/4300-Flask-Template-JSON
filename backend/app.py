02a079156050d3b8e037e8ab42d7554ce177c6e7
Marie Aractingi <ma788@cornell.edu>
2025-04-10 17:29:28 -0400

    filtered reviews further

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'sephora_product_df.json')
+json_file_path = os.path.join(current_directory, 'filtered_sephora_products.json')
 
 # Load the JSON data into a DataFrame
 with open(json_file_path, 'r') as file:
